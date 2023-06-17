876dd91b8753cc73c0c54e4da0356ef7dfe068b2
Rodolfo <>
2023-06-16 22:19:43 -0300

    changes allow srt file shift now

--- correction.py
+++ correction.py
@@ -9,7 +9,8 @@ def funcao_positiva(nome_arquivo:str,segundos:int,operator:bool,sub_type:bool):
     # print(list(map(lambda x: x.split('-->'),final)))
     for x in final:
         if(x.__contains__('-->')):
-            result = (x.replace(' ','').replace(',','').replace('\n','').split('-->'))
+            result = (x.replace(' ','').replace(',','.').replace('\n','').split('-->'))
+            # print(result)
             if(len(result[0].split(':')) == 2):
                 if(operator):
                     final1 = [datetime.strptime(result[0],'%M:%S.%f') + timedelta(seconds=segundos),datetime.strptime(result[1],'%M:%S.%f') + timedelta(seconds=segundos)]
@@ -18,6 +19,8 @@ def funcao_positiva(nome_arquivo:str,segundos:int,operator:bool,sub_type:bool):
                 #NOTE print(f"primeiro valor {datetime.strftime(final[0],'%M:%S.%f')[:-3]}  Segundo valor {datetime.strftime(final[1],'%M:%S.%f')[:-3]}")
                 x = x.replace(result[0],datetime.strftime(final1[0],'%M:%S.%f')[:-3])
                 x = x.replace(result[1],datetime.strftime(final1[1],'%M:%S.%f')[:-3])
+                if(sub_type == False):
+                    x = x.replace('.',',')
                 final[counter] = x
             if(len(result[0].split(':')) == 3):
                 if(operator):
@@ -25,8 +28,11 @@ def funcao_positiva(nome_arquivo:str,segundos:int,operator:bool,sub_type:bool):
                 else:
                     final2 = [datetime.strptime(result[0],'%H:%M:%S.%f') - timedelta(seconds=segundos),datetime.strptime(result[1],'%H:%M:%S.%f') - timedelta(seconds=segundos)]
                 #NOTE  print(f"primeiro valor {datetime.strftime(final2[0],'%H:%M:%S.%f')[:-3]}  Segundo valor {datetime.strftime(final2[1],'%H:%M:%S.%f')[:-3]}")
-                x = x.replace(result[0],datetime.strftime(final2[0],'%M:%S.%f')[:-3])
-                x = x.replace(result[1],datetime.strftime(final2[1],'%M:%S.%f')[:-3]) 
+                x = x.replace(',','.')
+                x = x.replace(result[0],datetime.strftime(final2[0],'%H:%M:%S.%f')[:-3])
+                x = x.replace(result[1],datetime.strftime(final2[1],'%H:%M:%S.%f')[:-3])
+                if(sub_type == False):
+                    x = x.replace('.',',')
                 final[counter] = x
         counter += 1
     final_name = splitext(nome_arquivo)[0]+'-syncd'+splitext(nome_arquivo)[1]
@@ -36,6 +42,7 @@ def funcao_positiva(nome_arquivo:str,segundos:int,operator:bool,sub_type:bool):
     
 
 nome_arquivo = 'KuroNeko-Subtitles.vtt'
+new_nome = 'I.Live.In.Fear.English-WWW.MY-SUBS.CO.srt'
 # funcao(nome_arquivo=nome_arquivo,segundos=0)
 
 # algo = 70
@@ -51,11 +58,12 @@ nome_arquivo = 'KuroNeko-Subtitles.vtt'
 # print(final)
 
 if __name__ == '__main__':
+    nome_arquivo = new_nome
     if(splitext(nome_arquivo)[1] == '.vtt'):
         temp = True
     else:
         temp = False
-    funcao_positiva(nome_arquivo=nome_arquivo,segundos=19,operator=True)
+    funcao_positiva(nome_arquivo=nome_arquivo,segundos=20,operator=True,sub_type=temp)
 
 
 
